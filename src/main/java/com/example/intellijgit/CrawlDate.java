942cd4b36f9ef82f6fbc29e6c9da0266d178fc61
alexgim <alexgim961101@gmail.com>
2023-04-19 13:17:16 +0900

    feat: CrawlDate 클래스 추가

---
+++ src/main/java/com/example/intellijgit/CrawlDate.java
@@ -0,0 +1,11 @@
+package com.example.intellijgit;
+
+import java.time.LocalDateTime;
+
+public class CrawlDate {
+    private LocalDateTime crawlDate;
+
+    public CrawlDate(LocalDateTime crawlDate) {
+        this.crawlDate = crawlDate;
+    }
+}
