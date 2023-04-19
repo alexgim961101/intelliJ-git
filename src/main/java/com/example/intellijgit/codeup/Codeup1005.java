09d89d4356e0783a26aa60fd21571c1ff87da4e6
alexgim <alexgim961101@gmail.com>
2023-04-19 17:17:16 +0900

    코드업 1005번 주석 수정 main

--- src/main/java/com/example/intellijgit/codeup/Codeup1005.java
+++ src/main/java/com/example/intellijgit/codeup/Codeup1005.java
@@ -1,5 +1,8 @@
 package com.example.intellijgit.codeup;
 
+/**
+ * 주석 수정 메인 111
+ * */
 public class Codeup1005 {
     public static void main(String[] args) {
         System.out.println("\"Hello World\"");
