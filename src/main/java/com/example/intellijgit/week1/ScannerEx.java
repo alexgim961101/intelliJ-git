10e0b81406de80aa9a9026e59e88be3d81459257
alexgim <alexgim961101@gmail.com>
2023-04-19 15:30:28 +0900

    feat: 스케너 연습 - ScannerEx 클래스 추가

---
+++ src/main/java/com/example/intellijgit/week1/ScannerEx.java
@@ -0,0 +1,13 @@
+package com.example.intellijgit.week1;
+
+import java.util.Scanner;
+
+public class ScannerEx {
+    public void readTwoNumberPlus() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(sc.nextInt() + sc.nextInt());
+    }
+
+
+}
