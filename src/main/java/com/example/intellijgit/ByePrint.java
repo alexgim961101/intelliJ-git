a8f802fff89b47bb2f5f3e155cd488748be2f89c
alexgim <alexgim961101@gmail.com>
2023-04-19 11:30:36 +0900

    feat: Bye 기능 추가 - ByePrint 추가

---
+++ src/main/java/com/example/intellijgit/ByePrint.java
@@ -0,0 +1,10 @@
+package com.example.intellijgit;
+
+public class ByePrint {
+    public static void print(){
+        System.out.println("bye");
+    }
+    public static void main(String[] args) {
+        print();
+    }
+}
