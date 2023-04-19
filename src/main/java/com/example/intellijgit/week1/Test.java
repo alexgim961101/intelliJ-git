33aefb6dfb10d8130b58ec53877c3cdfe8d92e9f
alexgim <alexgim961101@gmail.com>
2023-04-19 14:49:56 +0900

    feat: 주석 추가

--- src/main/java/com/example/intellijgit/week1/Test.java
+++ src/main/java/com/example/intellijgit/week1/Test.java
@@ -2,6 +2,9 @@ package com.example.intellijgit.week1;
 
 import java.io.IOException;
 
+/**
+ * 주석 추가
+ * */
 public class Test {
     public static void main(String[] args) throws IOException {
         BufferedReaderEx bufferedReaderEx = new BufferedReaderEx();
