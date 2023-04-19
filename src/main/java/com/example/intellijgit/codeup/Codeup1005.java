710cfd2cb69069b7ec8d5d819f61e1d0d9d704db
alexgim <alexgim961101@gmail.com>
2023-04-19 17:16:11 +0900

    코드업 1005주석 수정

--- src/main/java/com/example/intellijgit/codeup/Codeup1005.java
+++ src/main/java/com/example/intellijgit/codeup/Codeup1005.java
@@ -1,5 +1,7 @@
 package com.example.intellijgit.codeup;
-
+/**
+ * 주석 수정 practice
+ * */
 public class Codeup1005 {
     public static void main(String[] args) {
         System.out.println("\"Hello World\"");
