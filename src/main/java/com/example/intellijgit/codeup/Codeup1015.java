414341fb1b2e4e72a10b50e99d756123f447bdfa
alexgim <alexgim961101@gmail.com>
2023-04-20 15:52:11 +0900

    feat: 코드업 1015번 문제 해결

---
+++ src/main/java/com/example/intellijgit/codeup/Codeup1015.java
@@ -0,0 +1,11 @@
+package com.example.intellijgit.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1015 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float num = sc.nextFloat();
+        System.out.printf("%.2f", num);
+    }
+}
