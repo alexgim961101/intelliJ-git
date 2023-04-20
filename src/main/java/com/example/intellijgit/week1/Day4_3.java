b5fc95cb7f75f1e165c81712030eaec85ea63aa7
alexgim <alexgim961101@gmail.com>
2023-04-20 13:43:14 +0900

    구구단 연습

---
+++ src/main/java/com/example/intellijgit/week1/Day4_3.java
@@ -0,0 +1,14 @@
+package com.example.intellijgit.week1;
+
+public class Day4_3 {
+    public static void main(String[] args) {
+        System.out.printf("%d * %d = %d\n", 2, 2, 4);
+        System.out.printf("%d-%02d-%02d\n", 2023, 4, 2);
+        System.out.printf("%.06f", 1.4);
+
+        System.out.println("----------------------------------------");
+        for(int i = 1; i <= 9; i++) {
+            System.out.printf("2 * %d = %d\n", i, 2 * i);
+        }
+    }
+}
