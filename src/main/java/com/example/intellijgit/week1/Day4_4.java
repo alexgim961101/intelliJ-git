1658fcb9f8e021ec8b0dc6764330d3055e0523b9
alexgim <alexgim961101@gmail.com>
2023-04-20 14:49:17 +0900

    feat: Day4_4 실습

---
+++ src/main/java/com/example/intellijgit/week1/Day4_4.java
@@ -0,0 +1,14 @@
+package com.example.intellijgit.week1;
+
+import java.util.Scanner;
+
+public class Day4_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int val1 = sc.nextInt();
+        int val2 = sc.nextInt();
+
+        System.out.printf("%d와 %d의 합은 %d입니다", val1, val2, val1 + val2);
+    }
+}
