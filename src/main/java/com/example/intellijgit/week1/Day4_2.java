38ac700e8a283487aedede23d7ba7bdfd6cdbb3b
alexgim <alexgim961101@gmail.com>
2023-04-20 09:41:36 +0900

    feat: 문자 2개 합치기 완료

---
+++ src/main/java/com/example/intellijgit/week1/Day4_2.java
@@ -0,0 +1,14 @@
+package com.example.intellijgit.week1;
+
+import java.util.Scanner;
+
+public class Day4_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str1 = sc.next();
+        String str2 = sc.next();
+
+        System.out.println(str1 + " " + str2);
+    }
+}
