605684c0408303e01da886c54d821a2017113533
alexgim <alexgim961101@gmail.com>
2023-04-20 10:28:37 +0900

    코드업 1010 ~ 1012 해결

---
+++ src/main/java/com/example/intellijgit/codeup/Codeup1010.java
@@ -0,0 +1,14 @@
+package com.example.intellijgit.codeup;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Codeup1010 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+
+        System.out.println(num);
+    }
+}
