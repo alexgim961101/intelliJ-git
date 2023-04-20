79273d65eb8020ed6f75dcae02b05578afd7f806
alexgim <alexgim961101@gmail.com>
2023-04-20 15:34:49 +0900

    feat: 변수 선언과 초기화, 변수에 값 담아서 더하기, 코드업 1014 해결

---
+++ src/main/java/com/example/intellijgit/week1/Day4_5.java
@@ -0,0 +1,17 @@
+package com.example.intellijgit.week1;
+
+import java.util.Scanner;
+
+public class Day4_5 {
+    public static void main(String[] args) {
+        // 1. 변수 선언과 초기화
+        int val;
+        val = 0;
+
+        // 2. 변수에 값 하나씩 담아서 더하기
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        System.out.println(num1 + num2);
+    }
+}
