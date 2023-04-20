52af553d23f7572d0f077b5bfa48cbdea0a993f7
alexgim <alexgim961101@gmail.com>
2023-04-20 09:17:01 +0900

    feat: 복습

---
+++ src/main/java/com/example/intellijgit/week1/Day4_1.java
@@ -0,0 +1,11 @@
+package com.example.intellijgit.week1;
+
+import java.util.Scanner;
+
+public class Day4_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(sc.nextInt() + sc.nextInt());
+    }
+}
