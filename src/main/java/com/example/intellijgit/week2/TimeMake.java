635afb7b094cedea95335a84e1d97b3783d62c35
alexgim <alexgim961101@gmail.com>
2023-04-26 15:21:02 +0900

    feat: 분, 초 구하기

---
+++ src/main/java/com/example/intellijgit/week2/TimeMake.java
@@ -0,0 +1,11 @@
+package com.example.intellijgit.week2;
+
+import java.util.Date;
+
+public class TimeMake {
+    public static void main(String[] args) {
+        int sec = 239;
+        int min = sec / 60;
+        int rSec = 239 % 60;
+    }
+}
