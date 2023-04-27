1c945a8b0bef75ecf117ffea5f8943b7c2574787
alexgim <alexgim961101@gmail.com>
2023-04-27 15:17:02 +0900

    feat: Codeup 1059 ~ 1062 까지 해결 - Codeup 1059 ~ 1062.java 파일 추가

---
+++ src/main/java/com/example/intellijgit/codeup/Codeup1059.java
@@ -0,0 +1,13 @@
+package com.example.intellijgit.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1059 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        System.out.println(~num);
+    }
+}
