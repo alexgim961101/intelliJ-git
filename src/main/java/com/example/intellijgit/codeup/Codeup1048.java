28c5aceafb422344bd0295c0ba3660ddb8f6d363
alexgim <alexgim961101@gmail.com>
2023-04-27 14:30:40 +0900

    feat: Codeup 1047 ~ 1048 까지 해결 - Codeup 1047 ~ 1048.java 파일 추가

---
+++ src/main/java/com/example/intellijgit/codeup/Codeup1048.java
@@ -0,0 +1,14 @@
+package com.example.intellijgit.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1048 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        System.out.print(num1 << num2);
+    }
+}
