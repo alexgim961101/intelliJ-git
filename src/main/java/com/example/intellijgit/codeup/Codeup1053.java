c9443436391ba2707b580c30b81b139dc13f5918
alexgim <alexgim961101@gmail.com>
2023-04-27 13:33:16 +0900

    feat: Codeup 1053 ~ 1058 까지 해결 - Codeup 1053 ~ 1058.java 파일 추가

---
+++ src/main/java/com/example/intellijgit/codeup/Codeup1053.java
@@ -0,0 +1,13 @@
+package com.example.intellijgit.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1053 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        System.out.print(num == 0 ? 1 : 0);
+    }
+}
---
+++ src/main/java/com/example/intellijgit/codeup/Codeup1055.java
@@ -0,0 +1,14 @@
+package com.example.intellijgit.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1055 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        System.out.print(num1 == 1 || num2 == 1 ? 1 : 0);
+    }
+}
