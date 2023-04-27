d06a87750b9bb563188bd468e65174809167059d
alexgim <alexgim961101@gmail.com>
2023-04-27 16:10:02 +0900

    feat: CodeUp 1063 ~ 1064 해결 - Codeup1063 ~ 1064.java 파일 추가

---
+++ src/main/java/com/example/intellijgit/codeup/Codeup1063.java
@@ -0,0 +1,14 @@
+package com.example.intellijgit.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1063 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long num1 = sc.nextLong();
+        long num2 = sc.nextLong();
+
+        System.out.println(num1 > num2 ? num1 : num2);
+    }
+}
