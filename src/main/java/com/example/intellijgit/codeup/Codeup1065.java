9e0e069e4d438d038c0b2aaa4731e9ee51fdbca6
alexgim <alexgim961101@gmail.com>
2023-04-28 09:30:16 +0900

    feat: CodeUp 1065 ~ 1067 해결 - Codeup1065 ~ 1067.java 파일 추가

---
+++ src/main/java/com/example/intellijgit/codeup/Codeup1065.java
@@ -0,0 +1,19 @@
+package com.example.intellijgit.codeup;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Codeup1065 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        while (st.hasMoreTokens()) {
+            int num = Integer.parseInt(st.nextToken());
+
+            if(num % 2 == 0) System.out.println(num);
+        }
+    }
+}
