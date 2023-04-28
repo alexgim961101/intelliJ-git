0b00bf0d35d01257ee933ddb4a14ef08a18f604b
alexgim <alexgim961101@gmail.com>
2023-04-28 11:12:41 +0900

    feat: BubbleSort 예제 수정

--- src/main/java/com/example/intellijgit/week2/BubbleSort.java
+++ src/main/java/com/example/intellijgit/week2/BubbleSort.java
@@ -7,14 +7,18 @@ public class BubbleSort {
         int[] arr = {2, 1, 7, 9};
 
         for(int i = arr.length-1; i >= 0; i--){
+            boolean swap = false;
             for(int j = 0; j < i - 1; j++){
                 boolean check = arr[j] > arr[j+1];
                 if(check) {
+                    swap = true;
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
                 }
             }
+
+            if(!swap) break;
         }
 
         System.out.println(Arrays.toString(arr));
