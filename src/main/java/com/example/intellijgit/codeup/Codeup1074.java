4236cc4de75277d0ac5cccde709178e921c3f51c
alexgim <alexgim961101@gmail.com>
2023-05-01 11:51:20 +0900

    feat: 코드업 1073 ~ 1075 해결

---
+++ src/main/java/com/example/intellijgit/codeup/Codeup1074.java
@@ -0,0 +1,15 @@
+package com.example.intellijgit.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1074 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        for(int i = num; i >= 1; i--){
+            System.out.println(i);
+        }
+    }
+}
