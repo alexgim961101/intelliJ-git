e60bc051c8c8641ff43c97df48082fd84c02069b
alexgim <alexgim961101@gmail.com>
2023-05-08 09:59:00 +0900

    피라미드 해결

---
+++ src/main/java/com/example/intellijgit/week4/Pyramid.java
@@ -0,0 +1,17 @@
+package com.example.intellijgit.week4;
+
+public class Pyramid {
+    public static void main(String[] args) {
+        int num = 5;
+
+        for(int i = 1; i <= num; i++) {
+            for(int j = 0; j < num - i; j++) {
+                System.out.print(" ");
+            }
+            for(int j = 0; j < 2 * i - 1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
